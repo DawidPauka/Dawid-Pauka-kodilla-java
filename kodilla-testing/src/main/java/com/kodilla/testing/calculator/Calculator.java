a9054e7b9c266cfbb5bb73bb615d59802bfd2652
“nasz_login_do_github <nasz_email_do_github>
2018-06-07 20:09:05 +0200

    calculator testing

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+class SimpleCalculator{
+    public static int addAToB(int a, int b){
+        return a + b;
+    }
+
+    public static int substractAFromB(int a, int b){
+        return a - b;
+    }
+}
+
+
+
+ 
