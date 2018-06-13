da5ec045c4ab5bde63d205fdaa97e4b32ec489d2
“nasz_login_do_github <nasz_email_do_github>
2018-06-13 22:18:11 +0200

    6,3

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/SaySomething.java
@@ -0,0 +1,14 @@
+package com.kodilla.stream.lambda;
+
+
+
+
+public class SaySomething {
+
+    public void say() {
+            System.out.println("This is an example text.");
+        }
+
+
+    }
+
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,8 +1,8 @@
 package com.kodilla.testing.collection;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.BeforeClass;
 import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
@@ -14,6 +14,7 @@ public class CollectionTestSuite {
     public static void beforeClass() {
         System.out.println("Test Set: begin");
     }
+
     @AfterClass
     public static void afterClass() {
         System.out.println("Test Set: end");
@@ -21,26 +22,26 @@ public class CollectionTestSuite {
 
     @Test
 
-    public void testOddNumbersExterminatorEmptyList(){
+    public void testOddNumbersExterminatorEmptyList() {
 
         // Arrange
         OddNumbersExterminator exterminatorTest = new OddNumbersExterminator();
         // Act
-        ArrayList <Integer> newArrayListForClassOddNumbersExterminator = new ArrayList<>();
-        ArrayList <Integer> results = new ArrayList<>();
+        ArrayList<Integer> newArrayListForClassOddNumbersExterminator = new ArrayList<>();
+        ArrayList<Integer> results = new ArrayList<>();
         // Assert
         Assert.assertEquals(exterminatorTest.exterminate(newArrayListForClassOddNumbersExterminator), results);
-        }
+    }
 
     @Test
 
-    public void testOddNumbersExterminatorNormalList(){
+    public void testOddNumbersExterminatorNormalList() {
 
         // Arrange
         OddNumbersExterminator exterminatorTest = new OddNumbersExterminator();
 
 
-        ArrayList <Integer> newArrayListForClassOddNumbersExterminator = new ArrayList<>();
+        ArrayList<Integer> newArrayListForClassOddNumbersExterminator = new ArrayList<>();
 
         newArrayListForClassOddNumbersExterminator.add(2);
         newArrayListForClassOddNumbersExterminator.add(1);
@@ -49,15 +50,30 @@ public class CollectionTestSuite {
         newArrayListForClassOddNumbersExterminator.add(20);
 
         // Act
-        ArrayList <Integer> results = exterminatorTest.exterminate(newArrayListForClassOddNumbersExterminator);
-
+        ArrayList<Integer> results = exterminatorTest.exterminate(newArrayListForClassOddNumbersExterminator);
 
         // Assert
-        Assert.assertEquals( 2, 2);
-        Assert.assertEquals( 6, 6);
-        Assert.assertEquals( 4, 4);
-        Assert.assertEquals( 20, 20);
-        Assert.assertEquals( 1, 1);
 
+        Assert.assertEquals(4, results.size());
+
+        Assert.assertEquals(java.util.Optional.of(2), results.get(0));
+        Assert.assertEquals(java.util.Optional.of(6), results.get(1));
+        Assert.assertEquals(java.util.Optional.of(4), results.get(2));
+        Assert.assertEquals(java.util.Optional.of(20), results.get(3));
+
+
+        Assert.assertEquals(2, 2);
+        Assert.assertEquals(6, 6);
+        Assert.assertEquals(4, 4);
+        Assert.assertEquals(20, 20);
+        Assert.assertEquals(1, 1);
+
+    }
+
+
+    static public void assertEquals(String message, long expected, long actual) {
+        if (expected != actual) {
+            failNotEquals(message, Long.valueOf(expected), Long.valueOf(actual));
+        }
     }
 }
