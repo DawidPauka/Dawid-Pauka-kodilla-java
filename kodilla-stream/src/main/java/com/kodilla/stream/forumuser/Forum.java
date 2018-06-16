ea07edb4c9242f6503f04e21c1924e0d7adf5497
“nasz_login_do_github <nasz_email_do_github>
2018-06-16 18:47:18 +0200

    Corect line 8 in class Forum

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -4,31 +4,31 @@ import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 
-public final class userList {
-    private final List<ForumUser> UserList = new LinkedList<>();
+public final class Forum {
+    private final List<ForumUser> userList = new LinkedList<>();
 
-    public userList() {
-        UserList.add(
+    public Forum() {
+        userList.add(
                 new ForumUser("Paulina", 'M', LocalDate.parse("1864-05-15"), 22));
-        UserList.add(
+        userList.add(
                 new ForumUser("Bartek", 'M', LocalDate.parse("2012-12-30"), 3));
-        UserList.add(
+        userList.add(
                 new ForumUser("Ramzes", 'M', LocalDate.parse("1599-11-28"), 4));
-        UserList.add(
+        userList.add(
                 new ForumUser("Aleksandra", 'F', LocalDate.parse("1945-04-23"), 0));
-        UserList.add(
+        userList.add(
                 new ForumUser("Kendra", 'F', LocalDate.parse("2000-06-17"), 70));
-        UserList.add(
+        userList.add(
                 new ForumUser("Aleksander", 'M', LocalDate.parse("1689-05-25"), 1));
-        UserList.add(
+        userList.add(
                 new ForumUser("Dawid", 'M', LocalDate.parse("2000-07-15"), 32));
-        UserList.add(
+        userList.add(
                 new ForumUser("Angela", 'F', LocalDate.parse("1789-03-09"), 23));
-        UserList.add(
+        userList.add(
                 new ForumUser("Tomek", 'M', LocalDate.parse("1489-05-22"), 5));
     }
 
     public List<ForumUser> getUserList() {
-        return new LinkedList<>(UserList);
+        return new LinkedList<>(userList);
     }
 }
