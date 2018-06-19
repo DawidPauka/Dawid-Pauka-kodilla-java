4c27cb3bb372bda432352d83992eb4b9c1ff4176
“nasz_login_do_github <nasz_email_do_github>
2018-06-19 04:21:07 +0200

    Task 6.4  kolekcjonujemy figury (i testujemy to!)

---
+++ src/main/java/A.java
@@ -0,0 +1,14 @@
+public class A {
+    public String color;
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "color='" + color + '\'' +
+                '}';
+    }
+}
+
+
+
+
