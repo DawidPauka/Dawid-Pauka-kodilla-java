2e9f53e3d583216a4f76d14ce1ecc89fcd856b66
“nasz_login_do_github <nasz_email_do_github>
2018-07-21 12:09:11 +0200

    Zadanie: try! catch! finally!

---
+++ src/main/java/sdsdsdsdsd.java
@@ -0,0 +1,26 @@
+public class sdsdsdsdsd {
+    public static void main(String[] args) {
+
+        sdsdsdsdsd hey = new sdsdsdsdsd();
+        sdsdsdsdsd hey1 = hey;
+        sdsdsdsdsd solaksos = hey;
+
+        sdsdsdsdsd a2 = new sdsdsdsdsd();
+        sdsdsdsdsd dk = a2;
+        sdsdsdsdsd a = new sdsdsdsdsd();
+
+
+
+        System.out.println(hey.toString());
+        System.out.println(hey1.toString());
+        System.out.println(a2.toString());
+        System.out.println(dk.toString());
+        System.out.println(a.toString());
+        System.out.println(solaksos.toString());
+
+    }
+}
+
+
+
+
