3a0961297bf56febf62033cd5be9c3ee9e18f9d9
“nasz_login_do_github <nasz_email_do_github>
2018-08-03 19:12:34 +0200

    Zadanie tworzymy tabele z zadaniami

--- kodilla-patterns/src/main/java/com/kodilla/patterns/listOfFlights/Flight.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/listOfFlights/Flight.java
@@ -1,24 +1,25 @@
 package flights;
 
 import java.time.LocalTime;
+import java.util.Objects;
 
 public class Flight {
-    private String flightNumber;
+    private String number;
     private String departureAirport;
     private LocalTime departureTime;
     private String arrivalAirport;
     private LocalTime arrivalTime;
 
-    public Flight(String flightNumber, String departureAirport, LocalTime departureTime, String arrivalAirport, LocalTime arrivalTime) {
-        this.flightNumber = flightNumber;
+    public Flight(String number, String departureAirport, LocalTime departureTime, String arrivalAirport, LocalTime arrivalTime) {
+        this.number = number;
         this.departureAirport = departureAirport;
         this.departureTime = departureTime;
         this.arrivalAirport = arrivalAirport;
         this.arrivalTime = arrivalTime;
     }
 
-    public String getFlightNumber() {
-        return flightNumber;
+    public String getNumber() {
+        return number;
     }
 
     public String getDepartureAirport() {
@@ -35,5 +36,23 @@ public class Flight {
 
     public LocalTime getArrivalTime() {
         return arrivalTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return Objects.equals(number, flight.number) &&
+                Objects.equals(departureAirport, flight.departureAirport) &&
+                Objects.equals(departureTime, flight.departureTime) &&
+                Objects.equals(arrivalAirport, flight.arrivalAirport) &&
+                Objects.equals(arrivalTime, flight.arrivalTime);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(number, departureAirport, departureTime, arrivalAirport, arrivalTime);
     }
 }
